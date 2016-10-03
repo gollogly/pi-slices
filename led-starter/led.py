7248f9e6ab3c331fc3670860066ecc7f2cf70ec7
zemertz <zemertz@gmail.com>
2016-10-03 20:43:44 +0100

    Create led.py

---
+++ led-starter/led.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
+
+GPIO.setup(17,GPIO.OUT)
+
+print "LED on"
+
+GPIO.output(17,GPIO.HIGH)
+
+time.sleep(2)
+print "LED off"
+
+GPIO.output(17,GPIO.LOW)
