521aaa7ce392c61928c1a336ea7791767e0b1092
zemertz <zemertz@gmail.com>
2016-10-15 21:55:19 +0100

    Fixed calculation, now distance sensor working :)

--- pi-remote-car/distance-sensor/distance.py
+++ pi-remote-car/distance-sensor/distance.py
@@ -29,7 +29,7 @@ while GPIO.input(ECHO)==1:
 pulse_duration = pulse_end - pulse_start
 
 # Take Speed of Sound / 2 is 17150 
-distance = pulse_duration x 17150                 
+distance = pulse_duration * 17150                 
 
 distance = round(distance, 2)
 
