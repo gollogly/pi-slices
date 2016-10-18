540ac6be5c7b3cefcd407c2385c23dd1265422f9
zemertz <zemertz@gmail.com>
2016-10-18 11:50:09 +0100

    Update distance.py
    Update from Example http://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/

--- pi-remote-car/distance-sensor/distance.py
+++ pi-remote-car/distance-sensor/distance.py
@@ -12,28 +12,43 @@ print "Distance Measurement In Progress"
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+def distance():
+  GPIO.output(TRIG, False)
+  print "Waiting For Sensor To Settle"
+  time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+  GPIO.output(TRIG, True)
+  # set Trigger after 0.01ms to LOW
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
   pulse_start = time.time()
+	pulse_end = time.time()
+
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
   
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
   
-pulse_duration = pulse_end - pulse_start
+  pulse_duration = pulse_end - pulse_start
 
-# Take Speed of Sound / 2 is 17150 
-distance = pulse_duration * 17150                 
-
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
+  # Take Speed of Sound / 2 is 17150 
+  distance = pulse_duration * 17150                 
+  distance = round(distance, 2)
+  print "Distance:",distance,"cm"
+  
+  return distance
 
 # Release GPIO connections
-GPIO.cleanup()                                    
+if __name__ == '__main__':
+	try:
+		while True:
+			dist = distance()
+			print ("Measured Distance = %.1f cm" % dist)
+			time.sleep(1)
+
+		# Reset by pressing CTRL + C
+	except KeyboardInterrupt:
+		print("Measurement stopped by User")
+		GPIO.cleanup()                                  
